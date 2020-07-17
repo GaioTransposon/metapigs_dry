447f10a365e1af55c5b5bb9a13166ff6f7e562b9
Daniela Gaio <madonnazzzzz.dg@gmail.com>
2020-07-17 21:02:22 +1000

    combine stats script added

---
+++ scripts/combine_stats.R
@@ -0,0 +1,21 @@
+
+###########################################################################################
+
+
+# open csv files from the out dir of the git repo
+
+
+# create workbook 
+wb <- createWorkbook()
+
+
+# save each csv file as a sheet of the workbook
+
+
+
+# save workbook in out dir (git repo) 
+
+
+
+
+###########################################################################################
