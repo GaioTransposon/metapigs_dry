79267da8dd0b029bb7a9e43024f6af6b912d8457
Daniela Gaio <madonnazzzzz.dg@gmail.com>
2020-12-09 21:19:42 +1100

    contig depths to counts

--- scripts/03_contigs_to_wa_contigs_HPC.R
+++ scripts/03_contigs_to_wa_contigs_HPC.R
@@ -1,36 +1,35 @@
-#!/usr/bin/Rscript
 # runs from the HPC 
-# old name: 1_5_HPC.R:
-# collects contigs from .fa and merges with depth.txt files
 # language: R 
-# version this script was developed in: "R version 3.6.0 (2019-04-26)"
-# platform this script was developed in: "x86_64-apple-darwin15.6.0"
 
 #This script requires the following packages:
 install.packages("base", repos = "http://cran.us.r-project.org")
 install.packages("data.table", repos = "http://cran.us.r-project.org", dependencies = TRUE)
 install.packages("dplyr", repos = "http://cran.us.r-project.org")
-install.packages("seqinr", repos = "http://cran.us.r-project.org")
 install.packages("stringr", repos = "http://cran.us.r-project.org")
 install.packages("utils", repos = "http://cran.us.r-project.org")
 install.packages("tidyr", repos = "http://cran.us.r-project.org")
+install.packages("plyr", repos = "http://cran.us.r-project.org")
+install.packages("readxl", repos = "http://cran.us.r-project.org")
 
 #upload all libraries
 library(base)
 library(data.table)
 library(dplyr)
-library(seqinr)
 library(stringr)
 library(utils)
 library(tidyr)
+library(plyr)
+library(readxl)
 
 # input dir
-pig.id.basedir = "/shared/homes/12705859/out_new"
-
+pig.id.basedir = "/shared/homes/12705859/out_new" # on HPC
+out.dir = "/shared/homes/12705859/contig_abundances" # on HPC
+#pig.id.basedir = "/Users/12705859/Desktop/bins_clustering_parsing_DFs/out_new_test" # on local
+#out.dir = "/Users/12705859/Desktop/bins_clustering_parsing_DFs/contig_abundances" # on local
 
 #calculating weighted averages for contigs and saving as wa_contigs.csv
 for (pig.id in list.files(pig.id.basedir)) {
-  pig.id.dir = file.path(pig.id.basedir, pig.id, "metabat")
+  pig.id.dir = file.path(pig.id.basedir, pig.id, "metabat") 
   
   old <- paste(pig.id.dir, "depth_with_bins.csv", sep="/")
   
@@ -58,3 +57,143 @@ for (pig.id in list.files(pig.id.basedir)) {
     row.names=FALSE
   )
 }
+
+# create empty df with all possible headers (dates) and interate through each sample's df
+# containing depths from weighted contigs 
+# concatenate all to one df > output as: new_headers_wa_contigs.csv
+for (pig.id in list.files(pig.id.basedir)) {
+  pig.id.dir = file.path(pig.id.basedir, pig.id, "metabat")
+  
+  df = data.frame(
+    pig = character(),
+    contigName = character(),
+    contigLen = character(),
+    X170130.01.bam = character(),
+    X170131.01.bam = character(),
+    X170131.02.bam = character(),
+    X170131.03.bam = character(),
+    X170201.01.bam = character(),
+    X170201.02.bam = character(),
+    X170203.01.bam = character(),
+    X170206.01.bam = character(),
+    X170206.02.bam = character(),
+    X170207.01.bam = character(),
+    X170207.02.bam = character(),
+    X170208.01.bam = character(),
+    X170210.01.bam = character(),
+    X170214.01.bam = character(),
+    X170214.02.bam = character(),
+    X170214.03.bam = character(),
+    X170216.01.bam = character(),
+    X170216.02.bam = character(),
+    X170217.01.bam = character(),
+    X170221.01.bam = character(),
+    X170224.01.bam = character(),
+    X170228.01.bam = character(),
+    X170303.01.bam = character(),
+    X170306.01.bam = character(),
+    X170307.01.bam = character(),
+    X170308.01.bam = character(),
+    X170309.01.bam = character(),
+    X170310.01.bam = character()
+  )
+  #if statement to exclude NegativeControl, MockCommunity, Protexin and Coliguard
+  if (grepl("[[:digit:]]", pig.id) == TRUE) {
+    old <- paste(pig.id.dir, "wa_contigs.csv", sep="/")
+    df2 <- read.table(file = old, header = TRUE, sep = ",", row.names = NULL)
+    
+    new_df <- rbind.fill(df,df2)
+    
+    write.csv(new_df, row.names=FALSE, quote=FALSE, file = file.path(pig.id.dir, "new_headers_wa_contigs.csv"))
+    
+    # fwrite(
+    #   x = new_df,
+    #   file = file.path(pig.id.dir, "new_headers_wa_contigs.csv"),
+    #   row.names=FALSE
+    # )
+  }
+  else {
+    print("not a pig subject")
+  }
+}
+
+
+
+df_total = data.frame()
+for (pig.id in list.files(pig.id.basedir)) {
+  pig.id.dir = file.path(pig.id.basedir, pig.id, "metabat")
+  #if statement to exclude NegativeControl, MockCommunity, Protexin and Coliguard
+  if (grepl("[[:digit:]]", pig.id) == TRUE) {
+    old <- paste(pig.id.dir, "new_headers_wa_contigs.csv", sep="/")
+    df2 <- read.table(file = old, header = TRUE, sep = ",", row.names = NULL)
+    
+    df3 <- data.frame(df2)
+    df_total <- rbind(df_total,df3)
+    
+    fwrite(
+      x = df_total,
+      file = file.path(out.dir,"merged_all_wa_contigs.csv"),
+      row.names=FALSE
+    )
+  }
+}
+
+
+# merge merged_all_clustered_wa_bins.csv with cohorts.xlsx (from metapigs/source_data)
+#input files
+cohorts <- read_excel("/shared/homes/12705859/cohorts.xlsx") # on HPC
+#cohorts <- read_excel("/Users/12705859/metapigs_dry/source_data/cohorts.xlsx") # on local
+df_total <- read.csv(file.path(out.dir,"merged_all_wa_contigs.csv")) 
+
+# reformat dates
+colnames(df_total)[colnames(df_total)=="X170130.01.bam"] <- "17-01-30.1"
+colnames(df_total)[colnames(df_total)=="X170131.01.bam"] <- "17-01-31.1"
+colnames(df_total)[colnames(df_total)=="X170131.02.bam"] <- "17-01-31.2"
+colnames(df_total)[colnames(df_total)=="X170131.03.bam"] <- "17-01-31.3"
+colnames(df_total)[colnames(df_total)=="X170201.01.bam"] <- "17-02-01.1"
+colnames(df_total)[colnames(df_total)=="X170201.02.bam"] <- "17-02-01.2"
+colnames(df_total)[colnames(df_total)=="X170203.01.bam"] <- "17-02-03.1"
+colnames(df_total)[colnames(df_total)=="X170206.01.bam"] <- "17-02-06.1"
+colnames(df_total)[colnames(df_total)=="X170206.02.bam"] <- "17-02-06.2"
+colnames(df_total)[colnames(df_total)=="X170207.01.bam"] <- "17-02-07.1"
+colnames(df_total)[colnames(df_total)=="X170207.02.bam"] <- "17-02-07.2"
+colnames(df_total)[colnames(df_total)=="X170208.01.bam"] <- "17-02-08.1"
+colnames(df_total)[colnames(df_total)=="X170210.01.bam"] <- "17-02-10.1"
+colnames(df_total)[colnames(df_total)=="X170214.01.bam"] <- "17-02-14.1"
+colnames(df_total)[colnames(df_total)=="X170214.02.bam"] <- "17-02-14.2"
+colnames(df_total)[colnames(df_total)=="X170214.03.bam"] <- "17-02-14.3"
+colnames(df_total)[colnames(df_total)=="X170216.01.bam"] <- "17-02-16.1"
+colnames(df_total)[colnames(df_total)=="X170216.02.bam"] <- "17-02-16.2"
+colnames(df_total)[colnames(df_total)=="X170217.01.bam"] <- "17-02-17.1"
+colnames(df_total)[colnames(df_total)=="X170221.01.bam"] <- "17-02-21.1"
+colnames(df_total)[colnames(df_total)=="X170207.01.bam"] <- "17-02-7.1"
+colnames(df_total)[colnames(df_total)=="X170224.01.bam"] <- "17-02-24.1"
+colnames(df_total)[colnames(df_total)=="X170228.01.bam"] <- "17-02-28.1"
+colnames(df_total)[colnames(df_total)=="X170303.01.bam"] <- "17-03-3.1"
+colnames(df_total)[colnames(df_total)=="X170306.01.bam"] <- "17-03-6.1"
+colnames(df_total)[colnames(df_total)=="X170307.01.bam"] <- "17-03-7.1"
+colnames(df_total)[colnames(df_total)=="X170308.01.bam"] <- "17-03-8.1"
+colnames(df_total)[colnames(df_total)=="X170309.01.bam"] <- "17-03-9.1"
+colnames(df_total)[colnames(df_total)=="X170310.01.bam"] <- "17-03-10.1"
+
+#merge cohorts
+df_total_2 <- merge.data.frame(df_total, cohorts, by.x="pig", by.y = "Animal ID")
+
+#move binLen column to first position of dataframe
+df_total_3 <- df_total_2 %>% 
+  dplyr::select("contigLen", everything())
+
+#move Cohort Name column to first position of dataframe
+df_total_4 <- df_total_3 %>% 
+  dplyr::select("Cohort Name", everything())
+
+#rename to eliminate space
+colnames(df_total_4)[colnames(df_total_4)=="Cohort Name"] <- "cohort"
+
+fwrite(
+  x = df_total_4,
+  file = file.path(out.dir,"merged_all_wa_contigs_with_cohorts.csv"), 
+  row.names=FALSE
+)
+
+
---
+++ scripts/04_contigs_depths_to_counts_HPC.R
@@ -0,0 +1,221 @@
+# runs from the HPC 
+# language: R 
+
+#This script requires the following packages:
+install.packages("base", repos = "http://cran.us.r-project.org")
+install.packages("data.table", repos = "http://cran.us.r-project.org", dependencies = TRUE)
+install.packages("dplyr", repos = "http://cran.us.r-project.org")
+install.packages("stringr", repos = "http://cran.us.r-project.org")
+install.packages("utils", repos = "http://cran.us.r-project.org")
+install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+install.packages("plyr", repos = "http://cran.us.r-project.org")
+install.packages("splitstackshape", repos = "http://cran.us.r-project.org")
+install.packages("reshape", repos = "http://cran.us.r-project.org")
+
+#upload all libraries
+library(base)
+library(data.table)
+library(dplyr)
+library(stringr)
+library(utils)
+library(tidyverse)
+library(plyr)
+library(splitstackshape)
+library(reshape)
+#library(magrittr)
+
+out.dir = "/shared/homes/12705859/contig_abundances" # on HPC
+#out.dir = "/Users/12705859/Desktop/bins_clustering_parsing_DFs/contig_abundances" # on local
+
+
+df0 <- read.table(file = file.path(out.dir,"merged_all_wa_contigs_with_cohorts.csv"), header = TRUE, sep = ",", row.names = NULL)
+
+df1 <- df0
+
+# cohort names edits: 
+df1$cohort <- plyr::mapvalues(as.character(df1$cohort), 
+                             from = c("Neomycin+D-scour","Neomycin+ColiGuard","D-scour"), 
+                             to = c("NeoD","NeoC","D-Scour"))
+
+# remove totAvgDepth column
+df1 <- df1 %>% 
+  dplyr::select(!totalAvgDepth)
+
+# move bin column as first (might be useful at later stages)
+df1 <- df1 %>%
+  select(bin, everything())
+
+# # get the column names before entering function
+# original_colnames <- colnames(df1)
+# 
+# # artificially inflate the contig lengths (it will help get counts away from zeros)
+# df1 <- df1 %>%
+#   dplyr::mutate(contigLen=contigLen*1000000)
+
+# transform depth values into counts
+# where {counts = depth*median contig length/300} (300 is the read pair length) 
+A <- function(x) x * median(df1[,3]) / 300 
+counts <- data.frame(df1[1:5], apply(df1[6:ncol(df1)],2, A) )
+
+# now we don't need contigLen anymore we can remove this column
+counts <- counts %>% 
+  dplyr::select(!contigLen)
+
+# make long (one value per row)
+counts_long <- reshape::melt(counts, id=c("bin", "cohort", "pig","contigName"))
+
+# remove NAs (NAs were automatically created when pivoting wide)
+counts_long <- na.omit(counts_long)
+
+
+
+########################################################
+
+# First de-replication : 
+
+# some info on contigs
+# hist(counts_long$value, xlim = c(0,1000000), breaks=1000)
+# summary(counts_long$value)
+
+# Merge the replicates (some samples are duplicate, same day and pig)
+# split column "variable" using dot separator
+NL2 <- cSplit(counts_long, "variable", ".")
+NL2$variable_1 <- as.numeric(gsub( "X", "", NL2$variable_1))
+NL2$collection_date <- paste0(NL2$variable_1,"-",NL2$variable_2,"-",NL2$variable_3)
+colnames(NL2)[colnames(NL2)=="variable_4"] <- "replicate"
+
+#reformat to be recognized as dates
+NL2$collection_date <- as.Date(NL2$collection_date, format = "%y-%m-%d")
+
+# checking how many replicates we have
+#return rows that have replicate==1, ==2, ==3, ==4
+NLrep1 <- NL2[which(NL2$replicate == 1), ]
+NLrep2 <- NL2[which(NL2$replicate == 2), ]
+NLrep3 <- NL2[which(NL2$replicate == 3), ]
+NLrep4 <- NL2[which(NL2$replicate == 4), ]
+NROW(NLrep1)
+NROW(NLrep2)
+NROW(NLrep3)
+NROW(NLrep4)
+#concatenate the above dataframes
+total <- rbind(NLrep1, NLrep2, NLrep3, NLrep4)
+NROW(total)
+
+# 1st de-replication (take mean out of replicates): 
+
+# remove replicate column
+NL2$replicate <- NULL
+
+no_reps_contigs <- stats::aggregate(NL2$value,by=list(cohort=NL2$cohort,
+                                              pig=NL2$pig,bin=NL2$bin, contigName=NL2$contigName,
+                                              collection_date=NL2$collection_date),data=NL2,FUN=mean)
+
+#replace column name x with "value"
+names(no_reps_contigs)[names(no_reps_contigs) == 'x'] <- 'value'
+
+##### TEST: 
+# checking if mean of replicates is working fine
+a <- dplyr::filter(NLrep1, pig == "29951", contigName == "k141_1000014", collection_date == "2017-01-31")
+a
+b <- dplyr::filter(NLrep2, pig == "29951", contigName == "k141_1000014", collection_date == "2017-01-31")
+b
+c <- dplyr::filter(no_reps_contigs, pig == "29951", contigName == "k141_1000014", collection_date == "2017-01-31")
+c
+
+# is the value of c the mean of a and b ?
+c$value==(a$value+b$value)/2
+# yes
+##### END OF TEST. 
+
+########################################################
+
+# Second de-replication : 
+
+# Important change 20191108: because some piglets could not be sampled on given sampling date
+# (or low quality sample was taken, so sample was taken again the next day),
+# they were re-sampled the morning after (before treatment was administered)
+# for this reason we are going to make groups of sampling dates as follows: 
+
+# duplicate date column 
+no_reps_contigs$date <- no_reps_contigs$collection_date 
+
+from = c("2017-01-30",
+         "2017-01-31","2017-02-01",
+         "2017-02-03",
+         "2017-02-06","2017-02-07","2017-02-08",
+         "2017-02-10",
+         "2017-02-14",
+         "2017-02-16","2017-02-17",
+         "2017-02-21",
+         "2017-02-24", 
+         "2017-02-28",
+         "2017-03-03",
+         "2017-03-06","2017-03-07","2017-03-08","2017-03-09","2017-03-10")
+
+to = c("tM",
+       "t0","t0",
+       "t1",
+       "t2","t2","t2",
+       "t3",
+       "t4",
+       "t5","t5",
+       "t6",
+       "t7", 
+       "t8",
+       "t9",
+       "t10","t10","t10","t10","t10")
+
+# replace collection dates (date format) with groups of collection dates (character format)
+no_reps_contigs$date <- plyr::mapvalues(as.character(no_reps_contigs$date), from, to)
+unique(no_reps_contigs$date)
+
+# select columns to keep
+no_reps_contigs <- no_reps_contigs %>%
+  dplyr::select(cohort,pig,bin,contigName,date,value)
+
+######################
+
+# 2nd de-replication: now there should be again replicates because we joined some dates. 
+# (take mean out of replicates)
+no_reps_contigs$sampleID <- paste0(no_reps_contigs$date,"_",
+                                   no_reps_contigs$cohort,"_",
+                                   no_reps_contigs$pig,"_",
+                                   no_reps_contigs$contigName,
+                                   no_reps_contigs$bin)
+NROW(no_reps_contigs) == length(unique(no_reps_contigs$sampleID))
+NROW(no_reps_contigs) 
+length(unique(no_reps_contigs$sampleID)) 
+
+# here above we can see the amount of replicates (same pig, same date)
+no_reps_contigs2 <- stats::aggregate(no_reps_contigs$value,
+                                     by=list(cohort=no_reps_contigs$cohort,
+                                             pig=no_reps_contigs$pig,
+                                             bin=no_reps_contigs$bin,
+                                             contigName=no_reps_contigs$contigName,
+                                             date=no_reps_contigs$date),
+                                     data=no_reps_contigs,FUN=mean)
+
+#replace column name x with "value"
+names(no_reps_contigs2)[names(no_reps_contigs2) == 'x'] <- 'value'
+
+# sanity check: 
+# number of unique pigs
+length(unique(paste0(no_reps_contigs2$pig)))
+
+# number of bins in total across all pigs
+length(unique(paste0(no_reps_contigs2$pig,no_reps_contigs2$bin)))
+
+# number of bins in total across all pigs
+length(unique(paste0(no_reps_contigs2$pig,no_reps_contigs2$contigName)))
+
+# # remove piglets with SD (swine dysentery): 
+# no_reps3 <- no_reps2 %>%
+#   filter(!pig=="29665"&
+#            !pig=="29865"&
+#            !pig=="29702")
+
+# Write out to play with it (this is everything)
+fwrite(x = no_reps_contigs2,
+       file = file.path(out.dir,"no_reps_contigs.csv"))
+
+
